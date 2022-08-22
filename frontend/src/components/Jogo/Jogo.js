353599761570a3a972bb7ff05e031400a2de19b7
asvinicius <you@example.com>
2022-08-22 10:16:40 -0500

    Imagens adicionadas ao código

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,9 +1,13 @@
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
+import pipe from "../../assets/pipe.png";
+import mario from "../../assets/mario.gif";
 
 function Jogo() {
     return <div className="jogo">
         <img className="nuvens" src={clouds} alt="Nuvens" />
+        <img className="canos" src={pipe} alt="Canos" />
+        <img className="mario" src={mario} alt="Mario" />
     </div>
 }
 
