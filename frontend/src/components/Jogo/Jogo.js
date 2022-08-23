00bf50c7bb5b2fb5906070d9a628bad692350ac9
asvinicius <you@example.com>
2022-08-23 08:42:09 -0500

    Animações em andamento

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -3,8 +3,17 @@ import clouds from "../../assets/clouds.png";
 import pipe from "../../assets/pipe.png";
 import gameover from "../../assets/game-over.png";
 import mario from "../../assets/mario.gif";
+import {useState} from "react";
 
 function Jogo() {
+
+    const [jumping, setJumping] = useState(false);
+
+    document.onkeydown = function(){
+        console.log("On Key Down");
+        jumping = true;
+    }
+
     return <div className="jogo">
         <img className="nuvens" src={clouds} alt="Nuvens" />
         <img className="canos" src={pipe} alt="Canos" />
